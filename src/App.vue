a801c5dcedb5946656e49094ae4071278761069e
yuKalasnikova2 <pmalian@list.ru>
2023-11-20 15:14:29 +0800

    add sass

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div class="container">
     <h1>Resume</h1>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.container {
+  max-width: 594px;
+  margin: 0 auto;
+}
+</style>
